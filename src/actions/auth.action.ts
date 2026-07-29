76717558bcd60557e0b71321b7705dd25b1ac8d0
Erhan KOÇ <erhankoc2@gmail.com>
2026-07-29 13:15:43 +0300

    resend kısmı ayarlandı

--- src/actions/auth.action.ts
+++ src/actions/auth.action.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import prisma from "@/lib/prisma";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { sendPasswordResetEmail } from "./email.action";
 
--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,4 +1,4 @@
 import { Resend } from 'resend';
 
 // Make sure to add RESEND_API_KEY to your .env file
-export const resend = new Resend(process.env.RESEND_API_KEY);
+export const resend = new Resend(process.env.RESEND_API_KEY || "re_dummy123456789");
